239e14c21b88f749d852d80b35006492283ecb67
Elías Milano <87205729+MilanoDevX@users.noreply.github.com>
2025-02-24 22:33:24 +0000

    exercise completed

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,29 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,7 +43,15 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        new_member = {
+            "id": member.get("id", self._generateId()),
+            "first_name": member.get("first_name"),
+            "last_name": self.last_name,
+            "age": member.get("age"),
+            "lucky_numbers": member.get("lucky_numbers")
+        }
+        self._members.append(new_member)
+        return new_member
 
     def delete_member(self, id):
         # fill this method and update the return
